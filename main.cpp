be086cff0e03bbd33ea5a6d2330d481f1bcfef1c
wangmuy <wangmuy@gmail.com>
2021-11-20 23:13:20 +0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "solution.h"
+
+using namespace std;
+
+int main(int, char**) {
+    Solution s;
+    return s.resolve();
+}
---
+++ solution.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int resolve() {
+        cout<<"hello world";
+        return 0;
+    }
+};
